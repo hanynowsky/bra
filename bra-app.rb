5f9db9890f867a1370142a3bc184a775ca2247c7
Hanine HAMZIOUI <hanynowsky@gmail.com>
2017-12-22 09:57:54 +0000

    Enable Web app logging in log/bra-app.log

--- bra-app.rb
+++ bra-app.rb
@@ -2,7 +2,20 @@ require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run_proc('bra-app', {:dir_mode => :normal, :dir => "/tmp/"}) do
+`touch #{pwd}/bra-app.log`
+
+@options = {
+	:dir_mode   => :normal,
+	:dir        => '/tmp/',
+	:multiple   => false,
+	:backtrace  => true,
+	:monitor    => false,
+	:log_dir    => "#{pwd}/log",
+	:log_output => true,
+	:output_logfilename => "bra-app.log",
+	:logfilename => "bra-app.log"
+}
+Daemons.run_proc('bra-app', @options) do
 	Dir.chdir(pwd)
 	exec "ruby ui.rb"
 end
